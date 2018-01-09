464780efe42678e0867244e06115e70ecdfb5586
Isabel Garcia <isabelgg22@gmail.com>
2018-01-09 11:34:34 +0100

    adds initial solution for fibonacci algorithm

---
+++ Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static int fibonacci(int n) {
+
+        // Complete the function.
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
+}
