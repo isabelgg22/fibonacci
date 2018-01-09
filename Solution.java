6629d4f47d58f135f1830e44404b9b2f7bc80c01
Isabel Garcia <isabelgg22@gmail.com>
2018-01-09 11:42:49 +0100

    adds the solution for the fibonacci algorithm

--- Solution.java
+++ Solution.java
@@ -4,7 +4,13 @@ public class Solution {
 
     public static int fibonacci(int n) {
 
-        // Complete the function.
+        if (n == 0) {
+          return 0;
+        } else if (n == 1) {
+          return 1;
+        } else {
+          return fibonacci(n - 1) + fibonacci(n - 2);
+        }
     }
 
     public static void main(String[] args) {
